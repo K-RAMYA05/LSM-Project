212c2e0d7445eddf39ea077a4751a0c35da0f41d
K-RAMYA05 <ramyak.2019@vitstudent.ac.in>
2021-10-18 21:17:06 +0530

    commit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || '3000',
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||'mongodb+srv://Ramya:k01ramya@cluster0.oacjx.mongodb.net/users?retryWrites=true&w=majority'||
     process.env.MONGO_HOST 
